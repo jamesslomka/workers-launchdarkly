236e2cf9a1b8e16588f6d7a1408930e9546cd7e1
James Slomka <james.slomka@gmail.com>
2024-01-08 16:33:45 -0500

    format

--- src/FeatureFlagClient.ts
+++ src/FeatureFlagClient.ts
@@ -1,16 +1,16 @@
 import { init, LDClient } from '@launchdarkly/cloudflare-server-sdk';
 
 export const initLdClient = async (ldClient: LDClient, LD_KV: KVNamespace): Promise<LDClient> => {
-	try {
-		if (!ldClient) {
-			console.log('No LD Client... initializing....');
-			const client: LDClient = await init('ADD_LD_CLIENT_SIDE_ID_HERE', LD_KV);
-			ldClient = await client.waitForInitialization();
-			console.log('LD init successful');
-		}
-		return Promise.resolve(ldClient);
-	} catch (error) {
-		console.error('Unable to init LD', error);
-		return Promise.reject();
-	}
+    try {
+        if (!ldClient) {
+            console.log('No LD Client... initializing....');
+            const client: LDClient = await init('ADD_LD_CLIENT_SIDE_ID_HERE', LD_KV);
+            ldClient = await client.waitForInitialization();
+            console.log('LD init successful');
+        }
+        return Promise.resolve(ldClient);
+    } catch (error) {
+        console.error('Unable to init LD', error);
+        return Promise.reject();
+    }
 };
--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,48 @@ import { LDClient } from '@launchdarkly/cloudflare-server-sdk';
 import { initLdClient } from './FeatureFlagClient';
 
 export interface Env {
-	LD_KV: KVNamespace;
+    LD_KV: KVNamespace;
 }
 
 let ldClient: LDClient;
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		ldClient = await initLdClient(ldClient, env.LD_KV);
-		const ffEval = await ldClient.variation('isWorkerEnabled', { key: 'guest' }, false);
+    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+        ldClient = await initLdClient(ldClient, env.LD_KV);
+        const ffEval = await ldClient.variation('isWorkerEnabled', { key: 'guest' }, false);
 
-		return new Response(buildHtml(ffEval), {
-			headers: { 'content-type': 'text/html;charset=UTF-8' },
-		});
-	},
+        return new Response(buildHtml(ffEval), {
+            headers: { 'content-type': 'text/html;charset=UTF-8' },
+        });
+    },
 };
 
 const buildHtml = (ffEvaluation: boolean): string => {
-	const content = '<p> isWorkerEnabled=' + ffEvaluation + '</p>';
+    const content = '<p> isWorkerEnabled=' + ffEvaluation + '</p>';
 
-	const html_style = `
-			html{width:100vw; height:100vh;}
-			body{padding:0; margin:0 !important;height:100%;}
-			#container {
-				display: flex;
-				flex-direction:column;
-				align-items: center;
-				justify-content: center;
-				height: 100%;
-				color: black;
-				font-family:sans-serif;
-			}`;
+    const html_style = `
+		html{width:100vw; height:100vh;}
+		body{padding:0; margin:0 !important;height:100%;}
+		#container {
+			display: flex;
+			flex-direction:column;
+			align-items: center;
+			justify-content: center;
+			height: 100%;
+			color: black;
+			font-family:sans-serif;
+		}`;
 
-	return  `
-			<!DOCTYPE html>
-			<head>
-				<title>LD <> WORKERS</title>
-			</head>
-			<body>
-				<style> ${html_style}</style>
-				<div id="container">
-					${content}
-				</div>
-			</body>
-        `;
-}
+    return `
+		<!DOCTYPE html>
+		<head>
+			<title>LD <> WORKERS</title>
+		</head>
+		<body>
+			<style> ${html_style}</style>
+			<div id="container">
+				${content}
+			</div>
+		</body>
+	`;
+};
